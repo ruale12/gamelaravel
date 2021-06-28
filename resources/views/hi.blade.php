9432da83dea3be79352d713af5535830ab772f62
rubens alexander rangel gomez <rualexander12@gmail.com>
2021-06-28 14:01:09 -0400

    first commit

---
+++ resources/views/hi.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>hola como estas: {{ $name }}</h1>
+
+  </body>
+</html>
